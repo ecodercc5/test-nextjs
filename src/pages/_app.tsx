c400a4de7e99e0004c98b0def206561955995c7e
Eric Chen <ericrcc588@gmail.com>
2023-02-17 16:33:37 -0500

    asdf

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import * as ASdf from "openai";
+import * as qwer from "firebase/app";
+import * as rtyu from "firebase-admin";
+import * as qwerqwer from "pdfjs-dist";
+
+const asdf = [ASdf, qwer, rtyu, qwer, qwer];
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
